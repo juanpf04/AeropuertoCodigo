9f3725becdf0b59f237b01d7ba2ffeccea51d994
juanpf04 <juanpf04@ucm.es>
2024-03-20 19:17:17 +0100

    controlador terminado

--- AirGest RSP/src/presentacion/controlador/Controlador.java
+++ AirGest RSP/src/presentacion/controlador/Controlador.java
@@ -1,41 +1,14 @@
-/**
- * 
- */
 package presentacion.controlador;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author javia
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public abstract class Controlador {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	private static Controlador instancia;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public static Controlador getInstance() {
-		// begin-user-code
-		// TODO Auto-generated method stub
+		if(instancia == null)
+			instancia = new ControladorImp();
 		return null;
-		// end-user-code
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param evento
-	* @param datos
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public abstract void accion(int evento, Object datos);
 }
