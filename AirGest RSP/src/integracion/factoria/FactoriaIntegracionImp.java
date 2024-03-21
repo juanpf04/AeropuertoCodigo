b6e5022dd56aa1cf2868d5c23cd7b35c1f408a09
jaceituno16 <javiacei@ucm.es>
2024-03-21 18:06:00 +0100

    TAvion y Factoria Integracion acabados

--- AirGest RSP/src/integracion/factoria/FactoriaIntegracionImp.java
+++ AirGest RSP/src/integracion/factoria/FactoriaIntegracionImp.java
@@ -1,10 +1,13 @@
 package integracion.factoria;
 
 import integracion.aerolinea.DAOAerolinea;
+import integracion.aerolinea.DAOAerolineaImp;
 import integracion.avion.DAOAvion;
+import integracion.avion.DAOAvionImp;
 import integracion.modelo.DAOModelo;
 import integracion.modelo.DAOModeloImp;
 import integracion.modeloAerolinea.DAOModeloAerolinea;
+import integracion.modeloAerolinea.DAOModeloAerolineaImp;
 
 public class FactoriaIntegracionImp extends FactoriaIntegracion {
 
@@ -12,39 +15,15 @@ public class FactoriaIntegracionImp extends FactoriaIntegracion {
 		return new DAOModeloImp();
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see FactoriaIntegracion#crearDAOAvion()
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public DAOAvion crearDAOAvion() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return new DAOAvionImp();
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see FactoriaIntegracion#crearDAOAeolinea()
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public DAOAerolinea crearDAOAeolinea() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return new DAOAerolineaImp();
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see FactoriaIntegracion#crearDAOModeloAerolinea()
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public DAOModeloAerolinea crearDAOModeloAerolinea() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return new DAOModeloAerolineaImp();
 	}
 }
--- AirGest RSP/src/negocio/avion/TAvion.java
+++ AirGest RSP/src/negocio/avion/TAvion.java
@@ -1,297 +1,131 @@
-/**
- * 
- */
+
 package negocio.avion;
 
 import java.util.Date;
 import org.json.JSONObject;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author javia
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public class TAvion {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private int id;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private int numAsientos;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private Date fechaFabricacion;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private String nombre;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private String matricula;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private boolean activo;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private int idAerolinea;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private int idModelo;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private int idHangar;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public int getId() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return 0;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public int getNumAsientos() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return 0;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public Date getFechaFabricacion() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public String getNombre() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public String getMatricula() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public boolean getActivo() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return false;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public TAvion() {
-		// begin-user-code
-		// TODO Auto-generated constructor stub
-		// end-user-code
+
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param numAsientos
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setNumAsientos(int numAsientos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param fechaFabricacion
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setFechaFabricacion(Date fechaFabricacion) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param nombre
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setNombre(String nombre) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param matricula
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setMatricula(String matricula) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param activo
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setActivo(boolean activo) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param idAerolinea
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setIdAerolinea(int idAerolinea) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param idModelo
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setIdModelo(int idModelo) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param idHangar
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setIdHangar(int idHangar) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param id
-	* @param numAsientos
-	* @param fechaFabricacion
-	* @param nombre
-	* @param matricula
-	* @param activo
-	* @param idAerolinea
-	* @param idModelo
-	* @param idHangar
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public TAvion(int id, int numAsientos, Date fechaFabricacion, String nombre, String matricula, boolean activo,
 			int idAerolinea, int idModelo, int idHangar) {
-		// begin-user-code
-		// TODO Auto-generated constructor stub
-		// end-user-code
+		this.id = id;
+		this.numAsientos = numAsientos;
+		this.fechaFabricacion = fechaFabricacion;
+		this.nombre = nombre;
+		this.matricula = matricula;
+		this.activo = activo;
+		this.idAerolinea = idAerolinea;
+		this.idModelo = idModelo;
+		this.idHangar = idHangar;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	public int getId() {
+		return this.id;
+	}
+
+	public int getNumAsientos() {
+		return this.numAsientos;
+	}
+
+	public Date getFechaFabricacion() {
+		return this.fechaFabricacion;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public String getMatricula() {
+		return this.matricula;
+	}
+
+	public boolean getActivo() {
+		return this.activo;
+	}
+	
+	public int getIdAerolinea() {
+		return this.idAerolinea;
+	}
+
+	public int getIdModelo() {
+		return this.idModelo;
+	}
+
+	public int getIdHangar() {
+		return this.idHangar;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setNumAsientos(int numAsientos) {
+		this.numAsientos = numAsientos;
+	}
+
+	public void setFechaFabricacion(Date fechaFabricacion) {
+		this.fechaFabricacion = fechaFabricacion;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+
+	public void setIdAerolinea(int idAerolinea) {
+		this.idAerolinea = idAerolinea;
+	}
+
+	public void setIdModelo(int idModelo) {
+		this.idModelo = idModelo;
+	}
+
+	public void setIdHangar(int idHangar) {
+		this.idHangar = idHangar;
+	}
+
 	public JSONObject toJSON() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		JSONObject jo = new JSONObject();
+
+		jo.put("id", this.id);
+		jo.put("numAsiento", this.numAsientos);
+		jo.put("fechaFabricacion", this.fechaFabricacion);
+		jo.put("nombre", this.nombre);
+		jo.put("matricula", this.matricula);
+		jo.put("activo", this.activo);
+		jo.put("idModelo", this.idModelo);
+		jo.put("idAerolinea", this.idAerolinea);
+		jo.put("idHangar", this.idHangar);
+
+		return jo;
 	}
 }
