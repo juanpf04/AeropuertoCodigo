c5fc6048d970f3731f90b74d3805f5f5b608d37f
juanpf04 <juanpf04@ucm.es>
2024-03-25 22:57:04 +0100

    fecha

--- AirGest RSP/src/integracion/avion/DAOAvionImp.java
+++ AirGest RSP/src/integracion/avion/DAOAvionImp.java
@@ -1,23 +1,22 @@
-/**
- * 
- */
 package integracion.avion;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import miscelanea.Utilidades;
 import negocio.avion.TAvion;
 
 public class DAOAvionImp implements DAOAvion {
 
 	public List<TAvion> consultarAvionesPorModelo(int idModelo) {
-		File carpeta = new File("recursos/avion");
+		File carpeta = new File(Utilidades.ruta("avion"));
 		File[] lista = carpeta.listFiles();
 
 		List<TAvion> aviones = new ArrayList<>();
@@ -26,7 +25,12 @@ public class DAOAvionImp implements DAOAvion {
 			try {
 				JSONObject data = new JSONObject(new JSONTokener(new FileReader(f)));
 				if (data.getInt("idModelo") == idModelo) {
-					aviones.add(new TAvion(data.getInt("id"), data.getInt("numAsientos"), null,
+					
+					JSONObject jo = data.getJSONObject("fechaFabricacion");
+					
+					LocalDate fecha = LocalDate.of(jo.getInt("anyo"), jo.getInt("mes"), jo.getInt("dia"));
+					
+					aviones.add(new TAvion(data.getInt("id"), data.getInt("numAsientos"), fecha,
 							data.getString("nombre"), data.getString("matricula"), data.getBoolean("activo"),
 							data.getInt("idAerolinea"), data.getInt("idModelo"), data.getInt("idHangar")));
 				}
@@ -39,7 +43,7 @@ public class DAOAvionImp implements DAOAvion {
 	}
 
 	public List<TAvion> consultarAvionesActivosPorModelo(int idModelo) {
-		File carpeta = new File("recursos/avion");
+		File carpeta = new File(Utilidades.ruta("avion"));
 		File[] lista = carpeta.listFiles();
 
 		List<TAvion> aviones = new ArrayList<>();
@@ -48,7 +52,12 @@ public class DAOAvionImp implements DAOAvion {
 			try {
 				JSONObject data = new JSONObject(new JSONTokener(new FileReader(f)));
 				if (data.getInt("idModelo") == idModelo && data.getBoolean("activo")) {
-					aviones.add(new TAvion(data.getInt("id"), data.getInt("numAsientos"), null,
+					
+					JSONObject jo = data.getJSONObject("fechaFabricacion");
+					
+					LocalDate fecha = LocalDate.of(jo.getInt("anyo"), jo.getInt("mes"), jo.getInt("dia"));
+					
+					aviones.add(new TAvion(data.getInt("id"), data.getInt("numAsientos"), fecha,
 							data.getString("nombre"), data.getString("matricula"), data.getBoolean("activo"),
 							data.getInt("idAerolinea"), data.getInt("idModelo"), data.getInt("idHangar")));
 				}
--- AirGest RSP/test/integracion/avion/DAOAvionImpTest.java
+++ AirGest RSP/test/integracion/avion/DAOAvionImpTest.java
@@ -1,5 +1,15 @@
 package integracion.avion;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+import miscelanea.Utilidades;
+
 public class DAOAvionImpTest {
 	
+	@Test
+	public void consultar_aviones_por_modelo_test() {
+		Utilidades.esTest();
+		DAOAvion da = new DAOAvionImp();
+		
+	}
 }
