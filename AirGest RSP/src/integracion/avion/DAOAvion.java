0b573ce59b84e9ba0fc3c1f7abeb210e6d395fb5
juanpf04 <juanpf04@ucm.es>
2024-03-25 19:41:26 +0100

    estructura fecha

--- AirGest RSP/src/integracion/avion/DAOAvion.java
+++ AirGest RSP/src/integracion/avion/DAOAvion.java
@@ -1,18 +1,9 @@
-/**
- * 
- */
 package integracion.avion;
 
 import java.util.List;
 
 import negocio.avion.TAvion;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author javia
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public interface DAOAvion {
 
 	public List<TAvion> consultarAvionesPorModelo(int idModelo);
