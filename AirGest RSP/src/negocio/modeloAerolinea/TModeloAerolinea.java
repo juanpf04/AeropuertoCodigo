8f3ced8dc7d6b5b109dbf18bc0b66c2ae89f3024
jaceituno16 <javiacei@ucm.es>
2024-03-27 10:05:02 +0100

    TModeloAerolinea

--- AirGest RSP/src/negocio/modeloAerolinea/TModeloAerolinea.java
+++ AirGest RSP/src/negocio/modeloAerolinea/TModeloAerolinea.java
@@ -1,116 +1,51 @@
-/**
- * 
- */
+
 package negocio.modeloAerolinea;
 
 import org.json.JSONObject;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author Usuario
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public class TModeloAerolinea {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	private int idModelo;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	private int idAerolinea;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	public TModeloAerolinea() {
-		// begin-user-code
-		// TODO Auto-generated constructor stub
-		// end-user-code
+		
 	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public int getIdModelo() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return 0;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public int getIdAerolinea() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return 0;
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param idModelo
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setIdModelo(int idModelo) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param idAerolinea
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setIdAerolinea(int idAerolinea) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param idModelo
-	* @param idAerolinea
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	public TModeloAerolinea(int idModelo, int idAerolinea) {
-		// begin-user-code
-		// TODO Auto-generated constructor stub
-		// end-user-code
+		this.idModelo = idModelo;
+		this.idAerolinea = idAerolinea;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	public int getIdModelo() {
+		return this.idModelo;
+	}
+
+	public int getIdAerolinea() {
+		return this.idAerolinea;
+	}
+
+	public void setIdModelo(int idModelo) {
+		this.idModelo = idModelo;
+	}
+
+	
+	public void setIdAerolinea(int idAerolinea) {
+		this.idAerolinea = idAerolinea;
+	}
+
 	public JSONObject toJSON() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		JSONObject jo = new JSONObject();
+		
+		jo.put("idModelo", this.idModelo);
+		jo.put("idAerolinea", this.idAerolinea);
+		
+		return jo;
+	}
+	
+	public String toString(){
+		return "idModelo: " + this.idModelo + "\nidAerolinea: " + this.idAerolinea;
 	}
 }
