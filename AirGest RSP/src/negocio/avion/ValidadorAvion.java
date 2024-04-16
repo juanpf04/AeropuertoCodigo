5b9134e2caa41da835f1ddf824744a465f689f91
juanpf04 <juanpf04@ucm.es>
2024-04-16 18:33:56 +0200

    test vacios

--- AirGest RSP/src/negocio/avion/ValidadorAvion.java
+++ AirGest RSP/src/negocio/avion/ValidadorAvion.java
@@ -1,25 +1,8 @@
-/**
- * 
- */
 package negocio.avion;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author usuario_local
- * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public class ValidadorAvion {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	public static boolean comprobarDatos() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return false;
-		// end-user-code
+		return true; // TODO
 	}
 }
---
+++ AirGest RSP/tests/negocio/avion/SAAvionImpTest.java
@@ -0,0 +1,5 @@
+package negocio.avion;
+
+public class SAAvionImpTest {
+
+}
