7b11329e46aa9e679b987923cd9046dbd178ed06
vallop03 <vallop03@ucm.es>
2024-04-26 01:33:16 +0200

    mostrar por aerolinea (arreglar)

--- AirGest RSP/src/presentacion/avion/VistaMostrarAvionesPorAerolinea.java
+++ AirGest RSP/src/presentacion/avion/VistaMostrarAvionesPorAerolinea.java
@@ -1,27 +1,90 @@
-/**
- * 
- */
+
 package presentacion.avion;
 
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import presentacion.Observador;
+import presentacion.controlador.Controlador;
+import presentacion.controlador.EventosControlador;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author usuario_local
- * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public class VistaMostrarAvionesPorAerolinea extends JFrame implements Observador {
-	/** 
-	* (non-Javadoc)
-	* @see Observador#actualizaVista(Object datos)
-	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
+	public VistaMostrarAvionesPorAerolinea() {
+		super("Consultar aviones por aerolinea");
+		this.setSize(1000, 750);
+	}
+	
 	public void actualizaVista(Object datos) {
-		// begin-user-code
-		// TODO Auto-generated method stub
+		this.setIconImage(Toolkit.getDefaultToolkit().getImage("recursos/iconos/avion.png"));
+		JPanel principal = new JPanel();
+		principal.setLayout(new BoxLayout(principal, BoxLayout.PAGE_AXIS));
 
-		// end-user-code
+		JLabel funcion = new JLabel("Consultar aviones por aerolinea");
+		funcion.setFont(new Font("Times New Roman", Font.ITALIC, 85));
+
+		JPanel centro = new JPanel();
+		centro.setLayout(new BoxLayout(centro, BoxLayout.PAGE_AXIS));
+
+		JPanel idAerolinea = new JPanel();
+		idAerolinea.setLayout(new BoxLayout(idAerolinea, BoxLayout.LINE_AXIS));
+		JLabel etiquetaId = new JLabel("id Aerolinea: ");
+		etiquetaId.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		JTextField textoId = new JTextField();
+		textoId.setMaximumSize(new Dimension(200, 30));
+		textoId.setMinimumSize(new Dimension(200, 30));
+		textoId.setPreferredSize(new Dimension(200, 30));
+		idAerolinea.add(etiquetaId);
+		idAerolinea.add(textoId);
+		centro.add(idAerolinea);
+
+		Controlador controlador = Controlador.getInstance();
+
+		JButton aceptar = new JButton("ACEPTAR");
+		aceptar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					int id = Integer.parseInt(textoId.getText());
+					controlador.accion(EventosControlador.MOSTRAR_AVIONES_POR_AEROLINEA, id);
+				} catch (NumberFormatException n) {
+					controlador.accion(EventosControlador.MOSTRAR_AVIONES_POR_AEROLINEA, 0);
+				}
+			}
+		});
+
+		centro.add(aceptar);
+		principal.add(funcion);
+		principal.add(centro);
+
+		JButton atras = new JButton("ATRAS"); //boton para volver a la ventana principal
+		atras.setToolTipText("Esto vuelve a la ventana anterior");
+		atras.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				controlador.accion(EventosControlador.VISTA_AVION, null);
+			}
+		});
+
+		principal.add(atras);
+
+		this.setContentPane(principal);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setLocation(200, 200);
+		this.pack();
 	}
 }
