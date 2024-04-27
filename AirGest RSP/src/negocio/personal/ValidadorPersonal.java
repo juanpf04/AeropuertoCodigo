a4cade8077bf9f27b31c8a55e44ecc62b9f10bc9
juanpf04 <juanpf04@ucm.es>
2024-04-27 13:22:18 +0200

    personalHangar y validador personal

--- AirGest RSP/src/negocio/personal/ValidadorPersonal.java
+++ AirGest RSP/src/negocio/personal/ValidadorPersonal.java
@@ -3,7 +3,16 @@ package negocio.personal;
 public class ValidadorPersonal {
 
 	public static boolean comprobarDatos(TPersonal tPersonal) {
-		return true;
+		boolean valido = tPersonal.getIdEmpleado() > 0 && tPersonal.getAreaAsignada() != null
+				&& !tPersonal.getAreaAsignada().isEmpty();
+
+		if (tPersonal instanceof TPLimpieza)
+			valido = valido && ((TPLimpieza) tPersonal).getRol() != null
+					&& !((TPLimpieza) tPersonal).getRol().isEmpty();
+		else
+			valido = valido && ((TPSeguridad) tPersonal).getNumPlaca() > 0;
+
+		return valido;
 	}
 
 	public static boolean comprobarId(int id) {
--- AirGest RSP/src/negocio/personalHangar/TPersonalHangar.java
+++ AirGest RSP/src/negocio/personalHangar/TPersonalHangar.java
@@ -1,100 +1,32 @@
-/**
- * 
- */
 package negocio.personalHangar;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author usuario_local
- * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public class TPersonalHangar {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private int idPersonal;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private int idHangar;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public int getIdHangar() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return 0;
-		// end-user-code
+		return this.idHangar;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public int getIdPersonal() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return 0;
-		// end-user-code
+		return this.idPersonal;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param id
-	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public void setIdHangar(int id) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+		this.idHangar = id;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setIdPersonal() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	public void setIdPersonal(int id) {
+		this.idPersonal = id;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public TPersonalHangar() {
-		// begin-user-code
-		// TODO Auto-generated constructor stub
-		// end-user-code
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param idPersonal
-	* @param idHangar
-	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public TPersonalHangar(int idPersonal, int idHangar) {
-		// begin-user-code
-		// TODO Auto-generated constructor stub
-		// end-user-code
+		this.idPersonal = idPersonal;
+		this.idHangar = idHangar;
 	}
 }
